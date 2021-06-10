e570c4f5644e8718ae60f1e6ea7d099732985a6a
JoseJuanico <j.juanicopalma@gmail.com>
2021-06-09 23:00:47 -0400

    Se  configura archivo settings para conectarlo con oracle, se crean clases en archivo models, se genera migration a oracle de las clases creadas

--- Exp3_Juanico_001V/core/models.py
+++ Exp3_Juanico_001V/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    id = models.IntegerField (primary_key = True, verbose_name = 'Id de Usuario') #verbosename es lo que sale como un label en lapaginas
+    nombre = models.CharField (max_length = 20, verbose_name = 'Nombre usuario')
+    
+    def __str__(self): # metodo toString
+        return self.nombre
+
+class Admin(models.Model):
+    cargo = models.CharField(max_length = 7, primary_key = True, verbose_name = 'Cargo Admin')
+    id = models.ForeignKey (Usuario, on_delete=models.CASCADE) #se da relacion con usuario
+
+    def __str__(self):
+        return self.cargo
